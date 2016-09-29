d1abc3605bac126a2da357370e6b4cd336580766
Ana Teresa <ana_teresa30@hotmail.com>
2016-09-28 23:26:50 -0500

    pruebajSON5

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/api/:id',function(req, res,next){
 })
 
 router.get('/api1',function(req, res,next){
-	User.findOne({},{nombre:1, latitud:1, longitud:1},function(err,docs){
+	User.findOne({},{nombre, latitud, longitud},function(err,docs){
 		res.send(docs)
 	})
 })
